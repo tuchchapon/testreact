819c0414f7d3163c42277547044d3143681dc444
tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-01-21 15:13:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import CounterFunctionalComponent from "./CounterFunctionalComponent"
 import EX1style from "./components/ex1_style"
 import Ex2image from "./components/ex2_image"
 import Ex3prop from "./components/ex3_props"
+import Ex4Destrucring from "./components/ex4_destrurcturing"
 export default () =>{
   const [count, setCount] = useState(0)
+  const submit =() =>{
+    alert("hay")
+  }
   return (
     <div>
       JSX
@@ -19,7 +23,10 @@ export default () =>{
       <Ex2image/>
       <Ex2image image="/images/1.jpg"/>
       <Ex3prop count={count} color="#f00"showLabel/>
-
+      <Ex4Destrucring handleSubmit={submit}/>
+      <Ex4Destrucring handleSubmit={()=>alert("hay hay")}/>
+      
+    
     </div>
   );
 }
